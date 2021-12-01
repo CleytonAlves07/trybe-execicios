90dee35705194807664fcf847d56254da46c7dfa
Cleyton Alves <cleyton.alves.a@gmail.com>
2021-12-01 10:27:03 -0300

    Inclusao dos exercicos do dia 2 de introduçao a JavaScript

---
+++ Fundamentos/b4-javascript-primeiros-passos/d2-array-loop-for/scripts.js
@@ -0,0 +1,67 @@
+/* let taskList = ['Tomar café','Reunião','Brincar com o cachorro'];
+let searchForFirstTask = taskList[0];
+let searchForLastTask = taskList[taskList.length -1];
+let indexOfTask = taskList.indexOf('Reunião')
+
+taskList.push('Fazer execícios da Trybe')
+taskList.unshift('Acordar mais cedo');
+taskList.pop();
+taskList.shift();
+
+console.log(taskList);
+console.log(indexOfTask); */
+
+//Exercício 1
+/* let menu = ['Home', 'Serviços', 'Portifólio', 'Links'];
+let menuServices = menu.indexOf('Serviços');
+
+console.log(menuServices); */
+//Exercício 2
+/* let menu = ['Home','Serviços', 'Portifólios', 'Links'];
+let indexOfPortifolio = menu.indexOf('Portifólios');
+console.log(indexOfPortifolio);
+ */
+//Exercício 3
+/* let menu = ['Home', 'Serviços', 'Portifólio', 'Links'];
+menu.push('Contato');
+
+console.log(menu); */
+
+/* let tabuada = 9;
+for(let i=0;i<11;i++) {
+    console.log(`${tabuada} x ${i} = ${tabuada*i}`);
+} */
+/* let listaDeNomes = ['Joana', 'Maria', 'Lucas'];
+for(let i=0;i<listaDeNomes.length;i+=1){
+    console.log(`Olá, ${listaDeNomes[i]} !! Seja muito bem vindo(a)`);
+}
+ */
+/* let cars = ['Saab','Volvo', 'BMW'];
+for(let i=0;i<cars.length;i+=1){
+    console.log(cars[i]);
+} */
+/* let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for(let i=0;i<groceryList.length;i+=1){
+    console.log(groceryList[i]);
+} */
+/* let numeros = [1, 2, 3, 4, 5];
+for(let numero of numeros){
+    console.log(numero);
+} */
+/* let word = 'Hello';
+for(let letter of word) {
+    console.log(letter);
+}
+ */
+/* let arrOfNumbers =[10, 20, 30];
+for(let sum of arrOfNumbers){
+    sum +=1;
+    console.log(sum);
+} */
+let names = ['João','Maria','Antônio','Margarida'];
+for(let name of names){
+    console.log(`Olá, ${name}!`);
+}
+
+/* let firstNumber =Math.floor(Math.random()*60) +1; */
+
