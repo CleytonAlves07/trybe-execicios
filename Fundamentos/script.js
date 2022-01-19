16f93e6f8e8aaa4e075807a6ad3e9d5ff268b437
Cleyton Alves <cleyton.alves.a@gmail.com>
2022-01-19 10:20:34 -0300

    Just training higher-order-function

---
+++ Fundamentos/script.js
@@ -0,0 +1,250 @@
+// var age;
+
+// console.log(age); // undefined
+
+// age = 20;
+
+// const verifyIsNumber = (value1, value2) => {
+//     if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+//       throw new Error('Os valores devem ser numéricos');
+//     }
+//   };
+  
+//   const sum = (value1, value2) => {
+//     try {
+//       verifyIsNumber(value1, value2);
+//       return value1 + value2;
+//     } catch (error) {
+//       throw error.message;
+//     }
+//   };
+  
+//   console.log(sum(2, '3'));
+
+// const customer = {
+//   firstName: 'Roberto',
+//   age: 22,
+//   job: 'Teacher',
+// };
+
+// let newKey = 'lastName';
+// const lastName = 'Ferreira';
+// customer[newKey] = lastName;
+// newKey = 'fullName';
+// const fullName = `${customer.firstName} ${customer.lastName}`;
+// customer[newKey] = fullName;
+// console.log(customer);
+
+// const estruturaEmpresa = {
+//   rh: 'Fulana',
+//   financeiro: 'Ciclano',
+//   vendas: 'Beltrano',
+// };
+
+// let newKey = 'contabilidade';
+// const contabilidade = 'Ferreira';
+// estruturaEmpresa[newKey] = contabilidade;
+// newKey = 'contabilidade';
+// const finacas = `${estruturaEmpresa.financeiro} e ${estruturaEmpresa.contabilidade}`;
+// console.log(estruturaEmpresa);
+
+// const student1 = {
+//   Html: 'Muito Bom',
+//   Css: 'Bom',
+//   JavaScript: 'Ótimo',
+//   SoftSkills: 'Ótimo',
+// };
+
+// const student2 = {
+//   Html: 'Bom',
+//   Css: 'Ótimo',
+//   JavaScript: 'Ruim',
+//   SoftSkills: 'Ótimo',
+//   Git: 'Bom', // chave adicionada
+// };
+
+// console.log(`${Object.keys(student1)}: ${Object.values(student1)}`);
+
+// const listSkills = (student) => {
+//   const arrayOfSkills = Object.keys(student);
+//   for(index in arrayOfSkills){
+//     console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
+//   }
+// };
+
+// console.log('Estudante 1');
+// listSkills(student1);
+
+// console.log('Estudante 2');
+// listSkills(student2);
+
+// const student = {
+//   Html: 'Muito Bom',
+//   Css: 'Bom',
+//   JavaScript: 'Ótimo',
+//   SoftSkill: 'Ótimo',
+// };
+
+// const listSkillsValuesWithFor = (student) => {
+//   const skills = [];
+//   for(skill in student) {
+//     skills.push(student[skill]);
+//   }
+
+//   return skills;
+// };
+
+// const listSkillsValuesWithValues = (student) => Object.values(student);
+
+// const coolestTvShow = {
+//   name: "BoJack Horseman",
+//   genre: "adult animation",
+//   createdBy: "Raphael Bob-Waksberg",
+//   favoriteCharacter: "Princess Carolyn",
+//   quote: "Princess Carolyn always lands on her feet.",
+//   seasons: 6,
+// };
+
+// console.log(Object.entries(coolestTvShow));
+
+// [
+//   [ 'name', 'BoJack Horseman' ],
+//   [ 'genre', 'adult animation' ],
+//   [ 'createdBy', 'Raphael Bob-Waksberg' ],
+//   [ 'favoriteCharacter', 'Princess Carolyn' ],
+//   [ 'quote', 'Princess Carolyn always lands on her feet.' ],
+//   [ 'seasons', 6 ]
+// ]
+
+// // Sem Object.values
+// console.log(listSkillsValuesWithFor(student));
+
+// // Com Object.values
+// console.log(listSkillsValuesWithValues(student));
+
+// const países = {
+//   França: 'Paris',
+//   Brasil: 'Brasília',
+//   Espanha: 'Madrid',
+//   Portugal: 'Lisboa',
+// };
+// const pairKeyValue = Object.entries(países);
+// console.log(pairKeyValue);
+// for(index in pairKeyValue) {
+//   console.log('--------');
+//   console.log('País:', pairKeyValue[index][0]);
+//   console.log('Capital:', pairKeyValue[index][1]);
+// };
+
+// // A função recebe um número qualquer de parâmetros. Todos são agregados como valores para adicionar ao objeto de destino!
+
+// Object.assign(destino, objeto1);
+// Object.assign(destino, objeto1, objeto2);
+// Object.assign(destino, objeto1, objeto2, objeto3, objeto4);
+
+// const person = {
+//   name: 'Alberto',
+//   lastName: 'Gomes',
+//   age: 20,
+// };
+
+// const info = {
+//   age: 23,
+//   job: 'engenheiro',
+// };
+
+// const family = {
+//   children: ['Maria', 'João'],
+//   wife: 'Ana',
+// };
+
+// Object.assign(person, info, family)
+// console.log(person)
+
+/* Output
+  { name: 'Alberto',
+  lastName: 'Gomes',
+  age: 23,
+  job: 'engenheiro',
+  children: [ 'Maria', 'João' ],
+  wife: 'Ana'
+  } */
+
+//   const person = {
+//     name: 'Roberto',
+//   };
+  
+//   const lastName = {
+//     lastName: 'Silva',
+//   };
+  
+//   const clone = Object.assign(person, lastName);
+  
+//   console.log(clone); // { name: 'Roberto', lastName: 'Silva' }
+//   console.log(person); // { name: 'Roberto', lastName: 'Silva' }
+
+//   clone.name = 'Maria';
+
+// console.log('Mudando a propriedade name através do objeto clone')
+// console.log(clone); // Output: { name: 'Maria', lastName: 'Silva' }
+// console.log(person); // Output: { name: 'Maria', lastName: 'Silva' }
+// console.log('--------------');
+
+// person.lastName = 'Ferreira';
+
+// console.log('Mudando a propriedade lastName através do objeto person');
+// console.log(clone); // Output: { name: 'Maria', lastName: 'Ferreira' }
+// console.log(person); // Output: { name: 'Maria', lastName: 'Ferreira' }
+
+
+// const student1 = {
+//   Html: 'Muito Bom',
+//   Css: 'Bom',
+//   JavaScript: 'Ótimo',
+//   SoftSkills: 'Ótimo',
+// };
+// const student2 = {
+//   Html: 'Bom',
+//   Css: 'Ótimo',
+//   JavaScript: 'Ruim',
+//   SoftSkills: 'Ótimo',
+//   Git: 'Bom', // chave adicionada
+// };
+
+// student2.Redux = 'Muito ruim... sabe de nada';
+
+// console.log(`Estudante 1`);
+// for (let key of Object.entries(student1)) {
+//   console.log(` ${key[0]} : ${key[1]}`)
+// };
+// console.log(`Estudante 2`);
+// for(let key of Object.keys(student2)){
+//   console.log(`${key} : ${student2[key]}`)
+// }
+
+// const assert = require('assert');
+
+// assert.strictEqual(50, 50);
+// assert.strictEqual(50, 80);
+
+//Arrow function 
+// const getMultiplier = multiplier => aNumber => aNumber * multiplier;
+//Function simple
+// function getMultiplier (multiplier) {
+//   return (aNumber) => {
+//     return aNumber * multiplier;
+//   }
+// }
+// const double = getMultiplier(2);
+// const triple = getMultiplier(3);
+// const quadruple = getMultiplier(4);
+
+// console.log(triple(12));
+
+// const someGreeting = greeting => name => `Olá, ${name}! ${greeting}`;
+
+// const loveGreeting = someGreeting('Bonitão do meu coração');
+// const manoGreeting = someGreeting('firmeza, mano?');
+// const countryGreeting = someGreeting('cê tá bão?');
+
+// console.log(countryGreeting('Cleyton'));
