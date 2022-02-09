1fc9fe1db85c9af94b126fdf00db5740f02f97f9
Cleyton Alves <cleyton.alves.a@gmail.com>
2022-02-09 17:42:02 -0300

    Add

--- Fundamentos/b9-javascript-e-testes-assincronos/d3-jest-testes-assincronos/revisao/script.js
+++ Fundamentos/b9-javascript-e-testes-assincronos/d3-jest-testes-assincronos/revisao/script.js
@@ -1,18 +1,22 @@
 const section = document.querySelector('.hero');
+const button = document.querySelector('.button');
 const fullName = document.createElement('name');
 const image = document.createElement('img');
-const button = document.querySelector('.button');
 fullName.className = 'fullName'
 section.appendChild(fullName);
 section.appendChild(image);
 
+arrayIds = []
 
 const getId = () => {
     const randomId = Math.floor((Math.random() * 731 + 1));
     const randomToString = randomId.toString();
+    arrayIds.push(randomToString);
+    localStorage.setItem('ids', arrayIds);
     return randomToString;
 }
 button.addEventListener('click', () => hero(getId()));
+
 
 const hero = async (id) => {
     const url = `https://www.superheroapi.com/api.php/4192484924171229/${id}`;
@@ -26,4 +30,5 @@ const hero = async (id) => {
     image.innerHTML = src;
 }
 
+modules.export = getId;
 
