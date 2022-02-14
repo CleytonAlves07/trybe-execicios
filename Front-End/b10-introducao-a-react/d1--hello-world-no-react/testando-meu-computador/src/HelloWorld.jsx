f4bb3a3aa2eaa42c5fd117a5efc9ed667cf62a59
Cleyton Alves <cleyton.alves.a@gmail.com>
2022-02-14 14:54:08 -0300

    Added new exercises

---
+++ Front-End/b10-introducao-a-react/d1--hello-world-no-react/testando-meu-computador/src/HelloWorld.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class HelloWorld extends Component {
+    render() {
+        return <span>Só bora pru React</span>
+    }
+}
+
+export default HelloWorld
