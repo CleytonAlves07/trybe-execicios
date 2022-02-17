6b9d0e0316a4df2fef10965ca7de3395323721d8
Cleyton Alves <cleyton.alves.a@gmail.com>
2022-02-17 19:24:48 -0300

    Included propTypes

--- Front-End/b10-introducao-a-react/d2-componentes-react/my-pokedex/src/Components/Data.js
+++ Front-End/b10-introducao-a-react/d2-componentes-react/my-pokedex/src/Components/Data.js
@@ -1,33 +1,49 @@
 import { Component } from 'react';
 import Image from './Image';
+import PropTypes from 'prop-types';
 
-
-class pokemons extends Component {
+const { all } = this.props;
+class Pokemons extends Component {
   render() {   
     return (
       <div className='poke'>
       <div className='text'>
         <div className='name'>
-          <p> {this.props.pokemon.name}</p>
+          <p> {all.pokemon.name}</p>
         </div>
 
         <div className='tipo'>
-          <p> {this.props.pokemon.type}</p>
+          <p> {all.pokemon.type}</p>
         </div>
 
         <div className='peso'>
           <p>Peso:</p>
-          <p> {this.props.pokemon.averageWeight.value} {this.props.pokemon.averageWeight.measurementUnit}</p>
+          <p> {all.pokemon.averageWeight.value} {all.pokemon.averageWeight.measurementUnit}</p>
         </div>
 
       </div>
 
       <div className='image'>
-      <Image source={this.props.pokemon.image} alt='Foto Pokemon' />
+      <Image source={all.pokemon.image} alt='Foto Pokemon' />
       </div>
   </div>
     );
   }
 }
+
+Pokemons.propTypes = {
+  pokemons: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    type: PropTypes.string,
+    image: PropTypes.string,
+    moreInfo: PropTypes.string,
+    averageWeight: PropTypes.shape({
+      value: PropTypes.number,
+      measurementUnit: PropTypes.string,
+    })
+  })
+}
+
   
-export default pokemons;
+export default Pokemons;
