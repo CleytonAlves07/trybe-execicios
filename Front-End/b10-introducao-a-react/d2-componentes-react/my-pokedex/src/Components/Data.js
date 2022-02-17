fd114d3cf4b9443677e281ddf324d0c8d6141bc1
Cleyton Alves <cleyton.alves.a@gmail.com>
2022-02-17 18:41:24 -0300

    Added pokemon exercise

---
+++ Front-End/b10-introducao-a-react/d2-componentes-react/my-pokedex/src/Components/Data.js
@@ -0,0 +1,32 @@
+import { Component } from 'react';
+import Image from './Image';
+
+
+class pokemons extends Component {
+  render() {   
+    return (
+      <div className='poke'>
+      <div className='text'>
+        <div className='name'>
+          <p> {this.props.pokemon.name}</p>
+        </div>
+
+        <div className='tipo'>
+          <p> {this.props.pokemon.type}</p>
+        </div>
+
+        <div className='peso'>
+          <p> {this.props.pokemon.averageWeight.value} {this.props.pokemon.averageWeight.measurementUnit}</p>
+        </div>
+
+      </div>
+
+      <div className='image'>
+      <Image source={this.props.pokemon.image} alt='Foto Pokemon' />
+      </div>
+  </div>
+    );
+  }
+}
+  
+export default pokemons;
