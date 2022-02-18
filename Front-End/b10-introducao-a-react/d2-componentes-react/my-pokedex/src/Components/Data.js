88be8453908256c093bcf46b824ca3b7381d5a88
Cleyton Alves <cleyton.alves.a@gmail.com>
2022-02-18 09:56:12 -0300

    Removed the "all" destrution

--- Front-End/b10-introducao-a-react/d2-componentes-react/my-pokedex/src/Components/Data.js
+++ Front-End/b10-introducao-a-react/d2-componentes-react/my-pokedex/src/Components/Data.js
@@ -2,29 +2,29 @@ import { Component } from 'react';
 import Image from './Image';
 import PropTypes from 'prop-types';
 
-const { all } = this.props;
+
 class Pokemons extends Component {
   render() {   
     return (
       <div className='poke'>
       <div className='text'>
         <div className='name'>
-          <p> {all.pokemon.name}</p>
+          <p> {this.props.pokemon.name}</p>
         </div>
 
         <div className='tipo'>
-          <p> {all.pokemon.type}</p>
+          <p> {this.props.pokemon.type}</p>
         </div>
 
         <div className='peso'>
           <p>Peso:</p>
-          <p> {all.pokemon.averageWeight.value} {all.pokemon.averageWeight.measurementUnit}</p>
+          <p> {this.props.pokemon.averageWeight.value} {this.props.pokemon.averageWeight.measurementUnit}</p>
         </div>
 
       </div>
 
       <div className='image'>
-      <Image source={all.pokemon.image} alt='Foto Pokemon' />
+      <Image source={this.props.pokemon.image} alt='Foto Pokemon' />
       </div>
   </div>
     );
