9101f07ecbcaa8d2eeb50f407b3a78feb0457ec4
Cleyton Alves <cleyton.alves.a@gmail.com>
2022-02-21 18:43:16 -0300

    Added condicional and name of the color

--- Front-End/b11-componentes-com-estado-eventos-e-formularios-com-react/d1-componentes-com-estado-e-eventos/first-button/src/App.js
+++ Front-End/b11-componentes-com-estado-eventos-e-formularios-com-react/d1-componentes-com-estado-e-eventos/first-button/src/App.js
@@ -4,40 +4,50 @@ import './App.css';
 
 export default class App extends React.Component {
   constructor() {
-    super()
+    super();
 
-    this.handleClick = this.handleClick.bind(this);
+    this.handleClick1 = this.handleClick1.bind(this);
     this.handleClick2 = this.handleClick2.bind(this);
     this.handleClick3 = this.handleClick3.bind(this);
     this.state = {
-      numeroDeCliques: 0,
       numeroDeCliques1: 0,
-      numeroDeCliques2: 0
+      numeroDeCliques2: 0,
+      numeroDeCliques3: 0
     }
   }
-  handleClick() {
+  handleClick1() {
     // console.log(this);
     this.setState((estadoAnterior, _props) =>({
-      numeroDeCliques: estadoAnterior.numeroDeCliques + 1
-    }))
+      numeroDeCliques1: estadoAnterior.numeroDeCliques1 + 1
+    }), () => {
+      console.log(`Botão 1 ${this.colorButton(this.state.numeroDeCliques1)}`)
+    });
   }
   handleClick2() {
     this.setState((estadoAnterior, _props) => ({
-      numeroDeCliques1: estadoAnterior.numeroDeCliques1 + 1
-    }))
+      numeroDeCliques2: estadoAnterior.numeroDeCliques2 + 1
+    }), () => {
+      console.log(`Botão 2 ${this.colorButton(this.state.numeroDeCliques2)}`)
+    })
   }
   handleClick3() {
     this.setState((estadoAnterior, _props) => ({
-      numeroDeCliques2: estadoAnterior.numeroDeCliques2 + 1
-    }))
+      numeroDeCliques3: estadoAnterior.numeroDeCliques3 + 1
+    }), () => {
+      console.log(`Botão 3 ${this.colorButton(this.state.numeroDeCliques3)}`)
+    })
+  }
+  colorButton(num) {
+    return num % 2 === 0 ? "green" : "white";
   }
   render() {
     // console.log(this);
+    const {handleClick1, handleClick2, handleClick3} = this.state;
     return (
       <div>
-      <button type="button" onClick={this.handleClick}>{this.state.numeroDeCliques}</button>
-      <button type="button" onClick={this.handleClick2}>{this.state.numeroDeCliques1}</button>
-      <button type="button" onClick={this.handleClick3}>{this.state.numeroDeCliques2}</button>
+      <button type="button" onClick={this.handleClick1} style={{backgroundColor: this.colorButton(handleClick1)}}>{this.state.numeroDeCliques1} </button>
+      <button type="button" onClick={this.handleClick2} style={{backgroundColor: this.colorButton(handleClick2)}}>{this.state.numeroDeCliques2} </button>
+      <button type="button" onClick={this.handleClick3} style={{backgroundColor: this.colorButton(handleClick3)}}>{this.state.numeroDeCliques3}</button>
     </div>
     );
   }
