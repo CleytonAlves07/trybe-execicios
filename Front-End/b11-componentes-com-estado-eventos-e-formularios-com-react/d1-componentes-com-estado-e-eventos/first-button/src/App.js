1a764140c80b856e6b306f631cde4e9ba347e755
Cleyton Alves <cleyton.alves.a@gmail.com>
2022-02-21 14:58:43 -0300

    Added the other functions inside the class with this

--- Front-End/b11-componentes-com-estado-eventos-e-formularios-com-react/d1-componentes-com-estado-e-eventos/first-button/src/App.js
+++ Front-End/b11-componentes-com-estado-eventos-e-formularios-com-react/d1-componentes-com-estado-e-eventos/first-button/src/App.js
@@ -2,30 +2,30 @@ import React from 'react';
 import './App.css';
 
 
-function handleClick2() {
-  console.log("Clicou no botão 2!");
-}
-function handleClick3() {
-  console.log("Clicou no botão 3!");
-}
-
 export default class App extends React.Component {
   constructor() {
     super()
 
     this.handleClick = this.handleClick.bind(this);
+    this.handleClick2 = this.handleClick2.bind(this);
   }
   handleClick() {
     console.log(this);
     console.log("Clicou no botão 1!");
+  }
+  handleClick2() {
+    console.log("Clicou no botão 2!");
+  }
+  handleClick3() {
+    console.log("Clicou no botão 3!");
   }
   render() {
     console.log(this);
     return (
       <div>
       <button type="button" onClick={this.handleClick}>Botão 1</button>
-      <button type="button" onClick={handleClick2}>Botão 2</button>
-      <button type="button" onClick={handleClick3}>Botão 3</button>
+      <button type="button" onClick={this.handleClick2}>Botão 2</button>
+      <button type="button" onClick={this.handleClick3}>Botão 3</button>
     </div>
     );
   }
