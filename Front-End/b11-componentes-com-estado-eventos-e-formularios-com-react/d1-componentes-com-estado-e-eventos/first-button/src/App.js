665330893cbd4f498e68c1dcdcc9edfd9c99f2bf
Cleyton Alves <cleyton.alves.a@gmail.com>
2022-02-21 15:27:46 -0300

    Created more two buttons

--- Front-End/b11-componentes-com-estado-eventos-e-formularios-com-react/d1-componentes-com-estado-e-eventos/first-button/src/App.js
+++ Front-End/b11-componentes-com-estado-eventos-e-formularios-com-react/d1-componentes-com-estado-e-eventos/first-button/src/App.js
@@ -10,7 +10,9 @@ export default class App extends React.Component {
     this.handleClick2 = this.handleClick2.bind(this);
     this.handleClick3 = this.handleClick3.bind(this);
     this.state = {
-      numeroDeCliques: 0
+      numeroDeCliques: 0,
+      numeroDeCliques1: 0,
+      numeroDeCliques2: 0
     }
   }
   handleClick() {
@@ -20,18 +22,22 @@ export default class App extends React.Component {
     }))
   }
   handleClick2() {
-    console.log("Clicou no botão 2!");
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques1: estadoAnterior.numeroDeCliques1 + 1
+    }))
   }
   handleClick3() {
-    console.log("Clicou no botão 3!");
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques2: estadoAnterior.numeroDeCliques2 + 1
+    }))
   }
   render() {
-    console.log(this);
+    // console.log(this);
     return (
       <div>
       <button type="button" onClick={this.handleClick}>{this.state.numeroDeCliques}</button>
-      <button type="button" onClick={this.handleClick2}>Botão 2</button>
-      <button type="button" onClick={this.handleClick3}>Botão 3</button>
+      <button type="button" onClick={this.handleClick2}>{this.state.numeroDeCliques1}</button>
+      <button type="button" onClick={this.handleClick3}>{this.state.numeroDeCliques2}</button>
     </div>
     );
   }
