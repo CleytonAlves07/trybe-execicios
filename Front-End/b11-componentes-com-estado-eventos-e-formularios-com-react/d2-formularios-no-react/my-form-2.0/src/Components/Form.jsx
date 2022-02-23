f76300ddda8f0b06c2acc4a7847635c5271620f1
Cleyton Alves <cleyton.alves.a@gmail.com>
2022-02-23 11:10:37 -0300

    build: Included construtor and handle function

--- Front-End/b11-componentes-com-estado-eventos-e-formularios-com-react/d2-formularios-no-react/my-form-2.0/src/Components/Form.jsx
+++ Front-End/b11-componentes-com-estado-eventos-e-formularios-com-react/d2-formularios-no-react/my-form-2.0/src/Components/Form.jsx
@@ -1,58 +1,79 @@
 import React from 'react';
 
 export default class Form extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+        name: '',
+        email: '',
+        cpf: '',
+        cidade: '(AC)',
+        endereco: '',
+        estado: '',
+    }
+  }
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState ({
+        [name]: value,
+    })
+  }
   render() {
     return(
         <>
-        <label>Name:  </label>
-        <input type="text" name="name" required placeholder="Informe seu nome" maxLength="40"/>
+            <fieldset>
+                <label>Name:  </label>
+                <input type="text" name="name" required placeholder="Informe seu nome" maxLength="40"/>
 
-        <label>Email:  </label>
-        <input type="email" name="email" required placeholder="Informe seu email" maxLength="50"/>
+                <label>Email:  </label>
+                <input type="email" name="email" required placeholder="Informe seu email" maxLength="50"/>
 
-        <label>CPF:  </label>
-        <input type="text" name="cpf" required placeholder="CPF" maxLength="11" />
+                <label>CPF:  </label>
+                <input type="text" name="cpf" required placeholder="CPF" maxLength="11" />
 
-        <label>Cidade:  </label>
-        <input type="text" name="cidade" required placeholder="Informe a cidade"  maxLength="28"/>
+                <label>Cidade:  </label>
+                <input type="text" name="cidade" required placeholder="Informe a cidade"  maxLength="28"/>
 
-        <label>Endereço:  </label>
-        <input type="text" name="endereco" required placeholder="Informe o endereço" maxLength="200" />
+                <label>Endereço:  </label>
+                <input type="text" name="endereco" required placeholder="Informe o endereço" maxLength="200" />
 
-        <label>Estado:  </label>
-        <select required>
-            <option>Acre (AC)</option>
-            <option>Alagoas (AL)</option>
-            <option>Amapá (AP)</option>
-            <option>Amazonas (AM)</option>
-            <option>Bahia (BA)</option>
-            <option>Ceará (CE)</option>
-            <option>Distrito Federal (DF)</option>
-            <option>Espírito Santo (ES)</option>
-            <option>Goiás (GO)</option>
-            <option>Maranhão (MA)</option>
-            <option>Mato Grosso (MT)</option>
-            <option>Mato Grosso do Sul (MS)</option>
-            <option>Minas Gerais (MG)</option>
-            <option>Pará (PA)</option>
-            <option>Paraíba (PB)</option>
-            <option>Paraná (PR)</option>
-            <option>Pernambuco (PE)</option>
-            <option>Piauí (PI)</option>
-            <option>Rio de Janeiro (RJ)</option>
-            <option>Rio Grande do Norte (RN)</option>
-            <option>Rio Grande do Sul (RS)</option>
-            <option>Rondônia (RO)</option>
-            <option>Roraima (RR)</option>
-            <option>Santa Catarina (SC)</option>
-            <option>São Paulo (SP)</option>
-            <option>Sergipe (SE)</option>
-            <option>Tocantins (TO)</option>
-        </select>
-        <label>Informo o tipo de moradia:</label>
-        <input type="radio" value="Casa" name="radio" /> Casa
-        <input type="radio" value="Apartamento" name="radio"/> Apartamento
-
+                <label>Estado:  </label>
+                <select required>
+                    <option value="(AC)">Acre </option>
+                    <option value="(AL)">Alagoas </option>
+                    <option value="(AP)">Amapá </option>
+                    <option value="(AM)">Amazonas </option>
+                    <option value="(BA)">Bahia </option>
+                    <option value="(CE)">Ceará </option>
+                    <option value="(DF)">Distrito Federal </option>
+                    <option value="(ES)">Espírito Santo </option>
+                    <option value="(GO)">Goiás </option>
+                    <option value="(MA)">Maranhão </option>
+                    <option value="(MT)">Mato Grosso </option>
+                    <option value="(MS)">Mato Grosso do Sul </option>
+                    <option value="(MG)">Minas Gerais </option>
+                    <option value="(PA)">Pará </option>
+                    <option value="(PB)">Paraíba </option>
+                    <option value="(PR)">Paraná </option>
+                    <option value="(PE)">Pernambuco </option>
+                    <option value="(PI)">Piauí </option>
+                    <option value="(RJ)">Rio de Janeiro </option>
+                    <option value="(RN)">Rio Grande do Norte </option>
+                    <option value="(RS)">Rio Grande do Sul </option>
+                    <option value="(RO)">Rondônia </option>
+                    <option value="(RR)">Roraima </option>
+                    <option value="(SC)">Santa Catarina </option>
+                    <option value="(SP)">São Paulo </option>
+                    <option value="(SE)">Sergipe </option>
+                    <option value="(TO)">Tocantins </option>
+                </select>
+                <label>Informo o tipo de moradia:</label>
+                <input type="radio" value="Casa" name="radio" /> Casa
+                <input type="radio" value="Apartamento" name="radio"/> Apartamento
+            </fieldset>
         </>
        
     );
