449d501ae12ed3f3effa165dd0a4a3e2919245dd
Cleyton Alves <cleyton.alves.a@gmail.com>
2022-03-07 15:24:01 -0300

    build: Counter with phrases

--- Front-End/b12-ciclo-de-vida-de-componentes-e-react-router/timer/src/Components/Timer.jsx
+++ Front-End/b12-ciclo-de-vida-de-componentes-e-react-router/timer/src/Components/Timer.jsx
@@ -5,12 +5,24 @@ export default class Timer extends React.Component {
       super();
       this.state = {
         seconds: 0,
+        phases: ['😲 Inspire...', ' 😣 Segure...', '😮‍💨 Expire...'],
+        phasesIndex: 0,
       }
     }
+
+    componentDidMount() {
+      const oneSecond = 1000;
+      setInterval(() => {
+          this.setState((prevState) => ({
+              seconds: prevState.seconds + 1
+          }))
+      }, oneSecond)
+    }
   render() {
-      const { seconds } = this.state;
+      const { seconds, phases, phasesIndex } = this.state;
     return (
       <section>
+        <h1>{ phases[phasesIndex] }</h1>
         <h2>{seconds}</h2>
       </section>
     );
