b0f2597c76b44aff7bc00e366e87e5d27bc0b423
Cleyton Alves <cleyton.alves.a@gmail.com>
2022-03-25 12:43:29 -0300

    Explain more in the tutorial

--- Front-End/b15--gerenciamento-de-estado-com-redux/d1-introducao-ao-redux-o-estado-global-da-aplicacao/tutorial-redux/index.js
+++ Front-End/b15--gerenciamento-de-estado-com-redux/d1-introducao-ao-redux-o-estado-global-da-aplicacao/tutorial-redux/index.js
@@ -1,20 +1,25 @@
+//Inportar por aqui ou pelo html
 const Redux = require('redux');
 
+//Função que retorna uma action (ActionCreator)
 const fazerLogin = (email) => ({
   type: 'LOGIN',
   email
 });
 
+//Estado inicial 
 const ESTADO_INICIAL = {
   login: false,
   email: '',
 }
 
-
+//Reducer - Manipula o store seguindo as regras definidas pelas Actions
 const reducer = (state = ESTADO_INICIAL, action) => {
+  //Action para, se for o caso, executar no store
   switch (action.type) {
     case 'LOGIN':
       return {
+        //É importante colocar o spreed com o state, pois existe um comportamento de sobscrever 
         ...state,
         LOGIN: !state.login,
         email: action.email,
@@ -24,8 +29,11 @@ const reducer = (state = ESTADO_INICIAL, action) => {
   }
 };
 
+//Criação de um store para a função
 const store = Redux.createStore(reducer);
 
+//Despachando algo para o store
 store.dispatch(fazerLogin('alguem@email.com'));
 
+//Pegando o state do store 
 console.log(store.getState());
