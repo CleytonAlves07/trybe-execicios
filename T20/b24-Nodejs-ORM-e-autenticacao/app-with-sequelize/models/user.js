28710dbdb5efc644ebb372d1004477a6881b5519
Cleyton Alves <cleyton.alves.a@gmail.com>
2022-08-23 11:33:12 -0300

    added hello sequelize

---
+++ T20/b24-Nodejs-ORM-e-autenticacao/app-with-sequelize/models/user.js
@@ -0,0 +1,17 @@
+/**
+ * @param {import('sequelize').DataTypes} DataTypes
+ * @param {import('sequelize').Sequelize } Sequelize
+ * @returns
+ */
+
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    fullName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    phone_num: DataTypes.STRING,
+    
+  });
+  return User;
+}
+
+module.exports = User;
